b6a83dfe44b364f95d9205742ccfe29eff32dd7d
Brian LeRoux <b@brian.io>
2017-02-04 09:55:12 -0800

    hbd

---
+++ index.js
@@ -0,0 +1,23 @@
+var has = require('lodash.has')
+var property = require('lodash.property')
+var isBoolean = require('lodash.isboolean')
+var isError = require('lodash.iserror')
+var isFunction = require('lodash.isfunction')
+var isNumber = require('lodash.isnumber')
+var isObject = require('lodash.isobject')
+var isPlainObject = require('lodash.isplainobject')
+var isString = require('lodash.isstring')
+var isUndefined = require('lodash.isundefined')
+
+module.exports = { 
+  has,
+  property,
+  isBoolean,
+  isError,
+  isFunction,
+  isNumber,
+  isObject,
+  isPlainObject,
+  isString,
+  isUndefined
+}
